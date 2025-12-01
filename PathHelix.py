d0bc03d85834cb9befeaff0bf8ecf257611b9be6
Steven James <pyro@linuxlabs.com>
2025-11-30 21:36:27 -0500

    Fixing tangled PathHelix

--- PathHelix.py
+++ PathHelix.py
@@ -16,11 +16,22 @@
 #   Suite 330, Boston, MA  02111-1307, USA
 #                                                                         
 
-import Draft, Part, FreeCADGui
+import Part, FreeCADGui
 import FreeCAD as App
 import os
 from pathlib import Path
 from math import sin, cos, pi
+
+def radiusToGuide(p0,p1,guide):
+	N=p1-p0
+	N.normalize()
+	plane = Part.Plane(p0,N)
+
+	rp=guide.intersect(plane)[0]
+	# rp may be multiple points
+	rp = [p0.distanceToPoint(App.Vector(p.X,p.Y,p.Z)) for p in rp]	# convert Point to Vector
+
+	return min(rp)
 
 def ComputePlane2d(angle, radius):
 	y=sin(angle)*radius
@@ -33,38 +44,45 @@ def computeRadial(v0,v1, angle, radius):#	-- start and end are vectors represent
 	axis.normalize()
 	u=ComputePlane2d(angle,radius)
 
-	z=App.Vector(0,0,1)
+	z=App.Vector(0,0,1)	# since the plane lies on XY
 
-	y=axis.cross(z)
-	y.normalize()
+	y=axis.cross(z)		# compute u axis basis vector
+	print(axis*z)
+	try:
+		y.normalize()
 
-	t=axis.cross(y)
-	t.normalize()
+		t=axis.cross(y)		# compute x axis basis vector
+		t.normalize()
 
-	m=App.Matrix(t,y,axis)
-	v=m*u
-	return v0+v
+		m=App.Matrix(t,y,axis)	# build a matrix to project the UV space into 3D space
+		v=m*u
+	except App.Base.FreeCADError as e:
+		# u may need to be flipped over (mirrored) here in case the normals are near reverse of each other
+		print(e, z*axis)
+		v=u
+	return v0+v	# Apply the transformed vector so that the origin is on the path.
 
 
-def MakeHelix(path, pitch, radius, cont=0, rotation=0, direction=1, join=False):
+def MakeHelix(path, pitch, radius, cont=0, rotation=0, direction=1, join=False, Guide=None):
 	PathDistance=path.Length*4/pitch	# 4 sample points per turn
 	print("distance=",PathDistance)
 	pathPoints = path.discretize(Distance=path.Length/PathDistance)
-	print("pathPoints=", pathPoints)
+#	print("pathPoints=", pathPoints)
 
-#	radialLine = Draft.makeWire([ App.Vector(0,0,0), App.Vector(radius, 0.0, 0.0)], closed=False, face=False, support=None)
-#	radialLine.Visibility=False
 	radialPoints = []
 
 	angle=rotation
 	increment = -pi*direction/2
+	rad = radius
 	for i in range(len(pathPoints)-1):
-		radialPoints.append( computeRadial(pathPoints[i], pathPoints[i+1], angle, radius) )
+		if Guide:
+			rad = radiusToGuide(pathPoints[i], pathPoints[i+1], Guide.Curve)
+		radialPoints.append( computeRadial(pathPoints[i], pathPoints[i+1], angle, rad) )
 		angle = (angle+increment)%(2*pi)
 	if(cont):
 		i = len(pathPoints)-2
 		for x in range(cont):
-			radialPoints.append( computeRadial(pathPoints[i], pathPoints[i+1], angle, radius) )
+			radialPoints.append( computeRadial(pathPoints[i], pathPoints[i+1], angle, rad) )
 			angle = (angle+increment)%(2*pi)
 
 	arcs=[]
@@ -74,7 +92,6 @@ def MakeHelix(path, pitch, radius, cont=0, rotation=0, direction=1, join=False):
 		except:
 			pass
 
-#	radialLine.Document.removeObject(radialLine.Name)
 
 	if join:
 		bs = [ a.toBSpline(a.FirstParameter, a.LastParameter) for a in arcs ]
@@ -82,10 +99,10 @@ def MakeHelix(path, pitch, radius, cont=0, rotation=0, direction=1, join=False):
 		[ b.join(e) for e in bs[1:] ]
 		return b.toShape()
 
-	print("points:", radialPoints)
+#	print("points:", radialPoints)
 	shp=Part.Shape(arcs)
-	print(shp)
-	print(shp.Edges)
+#	print(shp)
+#	print(shp.Edges)
 	w = Part.Wire(shp.Edges)
 	print("W=",w)
 
@@ -100,6 +117,7 @@ class PathHelix:
 		obj.addProperty("App::PropertyFloat", "Pitch", "Dimensions")
 		obj.addProperty("App::PropertyFloat", "Rotation", "Dimensions")
 		obj.addProperty("App::PropertyLink", "Spine", "Dimensions")
+		obj.addProperty("App::PropertyLink", "Guide", "Dimensions")
 		obj.addProperty("App::PropertyBool", "ExtraHalf", "Dimensions").ExtraHalf=False
 		obj.addProperty("App::PropertyBool", "Reverse", "Dimensions").Reverse=False
 		obj.addProperty("App::PropertyBool", "Join", "Dimensions").Join=True
@@ -112,9 +130,15 @@ class PathHelix:
 		if (not hasattr(obj,"Join")):
 			obj.addProperty("App::PropertyBool", "Join", "Dimensions").Join=False
 
+		if (not hasattr(obj,"Guide")):
+			obj.addProperty("App::PropertyLink", "Guide", "Dimensions")
+
 	def execute(self, obj):
 		print("Spine=", obj.Spine.Shape)
-		w = MakeHelix(obj.Spine.Shape, obj.Pitch, obj.Radius, rotation=obj.Rotation*pi/180, cont=2 if(obj.ExtraHalf) else 0, direction= -1 if(obj.Reverse) else 1, join=obj.Join)
+		edge=None
+		if obj.Guide:
+			edge=obj.Guide.Shape.Edge1
+		w = MakeHelix(obj.Spine.Shape, obj.Pitch, obj.Radius, rotation=obj.Rotation*pi/180, cont=2 if(obj.ExtraHalf) else 0, direction= -1 if(obj.Reverse) else 1, join=obj.Join, Guide=edge)
 #		w.Placement = obj.Placement
 		obj.Shape=w
 #		Part.show(obj.Shape)
